e1710b27806015a4ea92facadca29999020c3d63
Bad-At-Naming <87281751+Bad-At-Naming@users.noreply.github.com>
2021-07-23 23:51:27 +0500

    Updated Main.py
    Removed the need to have to input the names of the subjects and now only require the links to be added to download all pdf files.

--- main.py
+++ main.py
@@ -5,40 +5,68 @@ import requests
 import re
 import os
 
-Subject_Links = []   #Put links of all subjects u want the pdfs from
-Subject_Names = []   #Put names of all subjects of the links u put above **IN THE SAME ORDER** as you did above
+import re
 
-for x in range(len(Subject_Names)):
-    os.mkdir("./"+Subject_Names[x])
+Subject_Links = [] # Make a list of all the links of the subjects you want to download pdfs of
 
-for n in range(len(Subject_Links)):   #Gets the past paper pdfs
-    With_Past_Papers = Subject_Links[n]+"/past-papers/"
-    html = urlopen(With_Past_Papers)
+def get_title(Link):
+    Link = Link.split("cambridge-o-level-")[1]
 
-    Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
-    soup = BeautifulSoup(html, 'html.parser')
-    links = []
-    for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
-        links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
 
-    for i in range(len(links)):
-        pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
-        Name_Of_Pdf_File = links[i].split("-",1)[1]
-        with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f: 
-            f.write(pdf.content) 
+    if Link[-1] == "/" :
+        Link = Link[:len(Link)-1]
 
-for n in range(len(Subject_Links)): #Gets the syllabus pdfs
-    With_Past_Papers = Subject_Links[n]
-    html = urlopen(With_Past_Papers)
+    Link = re.sub(r'[0-9]+', '', Link)
 
-    Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
-    soup = BeautifulSoup(html, 'html.parser')
-    links = []
-    for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
-        links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
+    if Link[-1] == "-" :
+        Link = Link[:len(Link)-1]
 
-    for i in range(len(links)):
-        pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
-        Name_Of_Pdf_File = links[i].split("-",1)[1]
-        with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f:
-            f.write(pdf.content)
+    return Link.title().replace("-"," ")
+
+Subject_Names = []
+def prep():
+    for n in range(len(Subject_Links)):
+        Subject_Names.append(get_title(Subject_Links[n]))
+
+    for x in range(len(Subject_Names)):
+        os.mkdir("./"+Subject_Names[x])
+
+
+
+def download_past_papers():
+    for n in range(len(Subject_Links)):   #Gets the past paper pdfs
+        With_Past_Papers = Subject_Links[n]+"/past-papers/"
+        html = urlopen(With_Past_Papers)
+
+        Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
+        soup = BeautifulSoup(html, 'html.parser')
+        links = []
+        for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
+            links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
+
+        for i in range(len(links)):
+            pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
+            Name_Of_Pdf_File = links[i].split("-",1)[1]
+            with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f: 
+                f.write(pdf.content) 
+
+def download_syllabus():
+    for n in range(len(Subject_Links)): #Gets the syllabus pdfs
+        With_Past_Papers = Subject_Links[n]
+        html = urlopen(With_Past_Papers)
+
+        Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
+        soup = BeautifulSoup(html, 'html.parser')
+        links = []
+        for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
+            links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
+
+        for i in range(len(links)):
+            pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
+            Name_Of_Pdf_File = links[i].split("-",1)[1]
+            with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f:
+                f.write(pdf.content)
+
+prep()
+download_past_papers()
+download_syllabus()
