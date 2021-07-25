abfba2c609476eff4f7ea0e7227ba11c0c36af0b
Bad-At-Naming <87281751+Bad-At-Naming@users.noreply.github.com>
2021-07-25 17:54:09 +0500

    Update main.py
    Made the syllabus and past papers pdfs be made in separate folders instead of both types of pdfs being in one folder.

--- main.py
+++ main.py
@@ -6,65 +6,84 @@ import re
 import os
 import re
 
-Subject_Links = [] # Make a list of all the links of the subjects you want to download pdfs of
+Subject_Links = ["https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-pakistan-studies-2059/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-islamiyat-2058/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-urdu-first-language-3247/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-literature-in-english-2010/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-english-language-1123/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-mathematics-d-4024/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-mathematics-additional-4037/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-physics-5054/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-chemistry-5070/", "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-o-level-biology-5090/"]  # Make a list of all the links of the subjects you want to download pdfs of
+
 
 def get_title(Link):
     Link = Link.split("cambridge-o-level-")[1]
 
-
-    if Link[-1] == "/" :
+    if Link[-1] == "/":
         Link = Link[:len(Link)-1]
 
     Link = re.sub(r'[0-9]+', '', Link)
 
-    if Link[-1] == "-" :
+    if Link[-1] == "-":
         Link = Link[:len(Link)-1]
 
-    return Link.title().replace("-"," ")
+    return Link.title().replace("-", " ")
+
 
 Subject_Names = []
+
+
 def prep():
+    os.mkdir("./Class X/")
     for n in range(len(Subject_Links)):
         Subject_Names.append(get_title(Subject_Links[n]))
 
     for x in range(len(Subject_Names)):
-        os.mkdir("./"+Subject_Names[x])
-
+        os.mkdir("./Class X/"+Subject_Names[x])
+        os.mkdir("./Class X/"+Subject_Names[x]+"/Syllabi")
+        os.mkdir("./Class X/"+Subject_Names[x]+"/Exam Papers")
 
 
 def download_past_papers():
-    for n in range(len(Subject_Links)):   #Gets the past paper pdfs
+    for n in range(len(Subject_Links)):  # Gets the past paper pdfs
+        print(f"starting {Subject_Names[n]} past papers")
         With_Past_Papers = Subject_Links[n]+"/past-papers/"
         html = urlopen(With_Past_Papers)
 
-        Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
+        # Used to actually access the pdf files
+        Base_Link = "https://www.cambridgeinternational.org"
         soup = BeautifulSoup(html, 'html.parser')
         links = []
         for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
-            links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
+            # Gets all pdf files from the page, needs to be appended to Base link to be accessed
+            links.append(str(link.get('href')))
 
         for i in range(len(links)):
-            pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
-            Name_Of_Pdf_File = links[i].split("-",1)[1]
-            with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f: 
-                f.write(pdf.content) 
+            # Appends pdf image link to base link to access pdf file
+            pdf = requests.get(Base_Link+links[i])
+            Name_Of_Pdf_File = links[i].split("-", 1)[1]
+            with open(("./Class X/"+Subject_Names[n]+"/Exam Papers/"+Name_Of_Pdf_File), 'wb') as f:
+                f.write(pdf.content)
+            print(f"done {Name_Of_Pdf_File}")
+        print(f"done {Subject_Names[n]} past papers")
+
 
 def download_syllabus():
-    for n in range(len(Subject_Links)): #Gets the syllabus pdfs
+    for n in range(len(Subject_Links)):  # Gets the syllabus pdfs
+        print(f"starting {Subject_Names[n]} syllabus")
         With_Past_Papers = Subject_Links[n]
         html = urlopen(With_Past_Papers)
 
-        Base_Link = "https://www.cambridgeinternational.org"   #Used to actually access the pdf files
+        # Used to actually access the pdf files
+        Base_Link = "https://www.cambridgeinternational.org"
         soup = BeautifulSoup(html, 'html.parser')
         links = []
         for link in soup.find_all(attrs={'href': re.compile(".pdf")}):
-            links.append(str(link.get('href')))   #Gets all pdf files from the page, needs to be appended to Base link to be accessed
+            # Gets all pdf files from the page, needs to be appended to Base link to be accessed
+            links.append(str(link.get('href')))
 
         for i in range(len(links)):
-            pdf = requests.get(Base_Link+links[i])   #Appends pdf image link to base link to access pdf file
-            Name_Of_Pdf_File = links[i].split("-",1)[1]
-            with open(("./"+Subject_Names[n]+"/"+Name_Of_Pdf_File),'wb') as f:
+            # Appends pdf image link to base link to access pdf file
+            pdf = requests.get(Base_Link+links[i])
+            Name_Of_Pdf_File = links[i].split("-", 1)[1]
+            with open(("./Class X/"+Subject_Names[n]+"/Syllabi/"+Name_Of_Pdf_File), 'wb') as f:
                 f.write(pdf.content)
+            print(f"done {Name_Of_Pdf_File}")
+        print(f"done {Subject_Names[n]} syllabi")
+
 
 prep()
 download_past_papers()
